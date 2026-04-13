ebb771dd66f7226208ff0bc0afc25454c083f7a3
xRookieFight <me@xrookiefight.com>
2026-04-13 16:07:50 +0300

    feat: update ResourcePackStackPacket

--- src/BedrockProtocol/Packets/ResourcePackStackPacket.cs
+++ src/BedrockProtocol/Packets/ResourcePackStackPacket.cs
@@ -1,4 +1,6 @@
 using BedrockProtocol.Utils;
+using BedrockProtocol.Packets.Types;
+using System.Collections.Generic;
 
 namespace BedrockProtocol.Packets
 {
@@ -6,27 +8,47 @@ namespace BedrockProtocol.Packets
     {
         public override uint PacketId => (uint)PacketIds.ResourcePackStack;
 
+        public List<ResourcePackStackEntry> ResourcePackStack { get; set; } = new();
         public bool MustAccept { get; set; }
-        public string GameVersion { get; set; } = string.Empty;
+        public string BaseGameVersion { get; set; } = string.Empty;
+        public Experiments Experiments { get; set; } = new();
+        public bool UseVanillaEditorPacks { get; set; }
 
         public override void Encode(BinaryStream stream)
         {
             stream.WriteBool(MustAccept);
-            stream.WriteUnsignedVarInt(0); 
-            stream.WriteUnsignedVarInt(0); 
-            stream.WriteString(GameVersion);
-            stream.WriteInt(0);
-            stream.WriteBool(false);
+
+            stream.WriteUnsignedVarInt((uint)ResourcePackStack.Count);
+            foreach (var entry in ResourcePackStack)
+            {
+                entry.Encode(stream);
+            }
+
+            stream.WriteString(BaseGameVersion);
+            Experiments.Encode(stream);
+            stream.WriteBool(UseVanillaEditorPacks);
         }
 
         public override void Decode(BinaryStream stream)
         {
             MustAccept = stream.ReadBool();
-            stream.ReadUnsignedVarInt(); 
-            stream.ReadUnsignedVarInt(); 
-            GameVersion = stream.ReadString();
-            stream.ReadInt();
-            stream.ReadBool();
+
+            uint count = stream.ReadUnsignedVarInt();
+            ResourcePackStack.Clear();
+
+            for (int i = 0; i < count; i++)
+            {
+                var entry = new ResourcePackStackEntry();
+                entry.Decode(stream);
+                ResourcePackStack.Add(entry);
+            }
+
+            BaseGameVersion = stream.ReadString();
+
+            Experiments = new Experiments();
+            Experiments.Decode(stream);
+
+            UseVanillaEditorPacks = stream.ReadBool();
         }
     }
 }
